af7a2f89460427d8bff0afaf0d7d35b3d3b442f1
Nishanth BS <nishanth.star1@gmail.com>
2018-12-13 08:04:10 +0530

    Add: welcome + header + initial sidebar components

---
+++ src/app/components/homepage/header/header.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { QueryServer } from 'src/app/services/queryServer.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  
+  private 
+
+  constructor(private queryServer : QueryServer) { }
+
+  ngOnInit() {
+  }
+
+  onQueryKeyup(queryString : string, enterClicked: boolean){
+    if(enterClicked){
+      this.queryServer.processSearchRequest(queryString);
+    }else{
+      if(queryString.length % 4 == 0){
+        this.queryServer.processSearchRequest(queryString);
+      }
+    }
+    
+  }
+}
